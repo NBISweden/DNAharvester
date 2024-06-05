8cf6eb82b041770b4e5d2274d386a586976216e9
verku <verena.kutschera@scilifelab.se>
2024-06-05 14:23:00 +0200

    Fix indentation and readability

--- read-harvester/bin/haplo2fasta.py
+++ read-harvester/bin/haplo2fasta.py
@@ -4,29 +4,34 @@ from itertools import izip, islice
 import gzip
 
 def parse_haplo(haplo_file):
+    # Define a list of sample names
+    samples = ["SF-1_42", "AI-1_119", "BL-11_9", "KU-23_3",
+               "MO34_ZA", "HD-3", "RY-3", "EL012", "Sheep", "domesticgoat"]
 
-  samples = ["SF-1_42","AI-1_119","BL-11_9","KU-23_3",
-        "MO34_ZA","HD-3","RY-3","EL012","Sheep","domesticgoat"]
+    # Create an empty dictionary to store the sequences for each sample
+    sample_dict = {}
+    for i in samples:
+        sample_dict[i] = ""
 
-  sample_dict	= {}
-  for	i in samples:
-    sample_dict[i] = ""
+    # Open the haplo_file using gzip and iterate over each line
+    with gzip.open(haplo_file) as f1:
+        next(f1)  # Skip the header line
+        for line in f1:
+            splitted = line.strip().split("\t")
+            chrom, pos, alleles = splitted[0], splitted[1], "".join(splitted[3:])
+            if "N" not in alleles:
+            if len(set(alleles)) == 2:
+                for i in range(len(alleles)):
+                focal_sample = samples[i]
+                sample_dict[focal_sample] += alleles[i]
 
-  with gzip.open(haplo_file) as f1:
-    next(f1)
-    for line in f1:
-      splitted = line.strip().split("\t")
-      chrom,pos,alleles = splitted[0],splitted[1],"".join(splitted[3:])
-      if "N" not in alleles:
-        if len(set(alleles)) == 2:
-          for	i in range(len(alleles)):
-            focal_sample = samples[i]
-            sample_dict[focal_sample] += alleles[i]
-
-  for key,value in sample_dict.items():
-    print ">" + key
-    print value
+    # Print the sequences for each sample
+    for key, value in sample_dict.items():
+        print(">" + key)
+        print(value)
 
 if __name__ == "__main__":
-  filename = argv[1]
-  parse_haplo(filename)
+    # Get the filename from command line arguments
+    filename = argv[1]
+    # Call the parse_haplo function with the filename
+    parse_haplo(filename)
