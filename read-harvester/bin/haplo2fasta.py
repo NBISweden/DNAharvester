2b71083f6252cabb3ae16513c6fde149551aef91
verku <verena.kutschera@scilifelab.se>
2024-09-18 14:55:11 +0200

    Rewrite for one sample at a time and keeping "N" to preserve genome coordinates

--- read-harvester/bin/haplo2fasta.py
+++ read-harvester/bin/haplo2fasta.py
@@ -4,32 +4,21 @@ from sys import argv
 from itertools import islice
 import gzip
 
-def parse_haplo(haplo_file):
-    # Define a list of sample names
-    samples = ["SF-1_42", "AI-1_119", "BL-11_9", "KU-23_3",
-               "MO34_ZA", "HD-3", "RY-3", "EL012", "Sheep", "domesticgoat"]
-
-    # Create an empty dictionary to store the sequences for each sample
+def parse_haplo(haplo_file, samplename):
+    # Create an empty dictionary to store the sequence with a samplename
     sample_dict = {}
-    for i in samples:
-        sample_dict[i] = ""
+    sample_dict[samplename] = ""
 
     # Open the haplo_file using gzip and iterate over each line
-    with gzip.open(haplo_file, 'rt') as f1:
-        next(f1)  # Skip the header line
-        for line in f1:
+    with gzip.open(haplo_file, 'rt') as f:
+        next(f)  # Skip the header line
+        for line in f:
             splitted = line.strip().split("\t")
             # Splits each line by tabs and extracts chrom, pos, and alleles.
-            # Alleles is formed by concatenating elements in the line starting from the fourth element onwards (splitted[3:]).
-            chrom, pos, alleles = splitted[0], splitted[1], "".join(splitted[3:])
-            # Filters out lines where alleles contains 'N'.
-            if "N" not in alleles:
-                # Checks if alleles contains exactly two unique values.
-                if len(set(alleles)) == 2:
-                    # Appends the respective allele (character) to the corresponding sample's sequence in sample_dict.
-                    for i in range(len(alleles)):
-                        focal_sample = samples[i]
-                        sample_dict[focal_sample] += alleles[i]
+            # Allele is extracted from column 3
+            chrom, pos, allele = splitted[0], splitted[1], splitted[3]
+            # Appends the respective allele (character) to the sample's sequence in sample_dict.
+            sample_dict[samplename] += allele
 
     # Print the sequences for each sample
     for key, value in sample_dict.items():
