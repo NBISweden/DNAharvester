3914193140ee33dac1d1ff9a540dc00ceded179b
bilalbioinfo <bilal.bioinfo@gmail.com>
2025-01-27 22:22:25 +0100

    added options to add library_type as single or double stranded library and fixed meta map where these info was being lostll!

--- bin/check_samplesheet.py
+++ bin/check_samplesheet.py
@@ -213,7 +213,7 @@ def check_samplesheet(file_in, file_out):
         https://raw.githubusercontent.com/nf-core/test-datasets/viralrecon/samplesheet/samplesheet_test_illumina_amplicon.csv
 
     """
-    required_columns = {"sample", "library_id", "lane", "library_type" "fastq_1", "fastq_2"}
+    required_columns = {"sample", "library_id", "lane", "library_type", "fastq_1", "fastq_2"}
     # See https://docs.python.org/3.9/library_id/csv.html#id3 to read up on `newline=""`.
     with file_in.open(newline="") as in_handle:
         reader = csv.DictReader(in_handle, dialect=sniff_format(in_handle))
